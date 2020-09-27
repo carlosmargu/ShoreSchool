e72bb0098a09fbacafc4e4f0bc7521284a7fa210
Carlos Martinez <camgxbox@live.com>
2020-09-26 23:10:24 -0500

    icono

--- src/components/Home/HomeComponent.js
+++ src/components/Home/HomeComponent.js
@@ -16,12 +16,12 @@ function Home(props) {
             </div>
         </Jumbotron>
       <div className="home-container container">
-        
+
         <div  className="container profile-data">
             <div className="row align-items-start">
                 <img className="profile-photo" src={props.imageUrl}/>
                 <div className="col-1 col-md m-1">
-                <h1 className="profile-user">{props.username} </h1>       
+                <h1 className="profile-user">{props.username} </h1>
                 <h4 className="">Grado 503</h4>
                 </div>
             </div>
@@ -32,16 +32,19 @@ function Home(props) {
                     <h5 className=""><span className="badge badge-secondary">27</span></h5>
                     <h6>OCT</h6>
                 </div>
-                <div className="col-10">
-                    <h3 className="text-uppercase"><strong>Operations Meeting</strong><a href={props.pdf} className="material-icons floating-btn boton1">cloud_download</a></h3>
+                <div className="col-8">
+                    <h3 className="text-uppercase"><strong>Operations Meeting</strong></h3>
                     <ul className="list-inline">
                         <li className="list-inline-item"><i className="fa fa-calendar-o" aria-hidden="true"></i> Friday</li>
                         <li className="list-inline-item"><i className="fa fa-clock-o" aria-hidden="true"></i> 2:30 PM - 4:00 PM</li>
                         <li className="list-inline-item"><i className="fa fa-location-arrow" aria-hidden="true"></i> Room 4019</li>
                     </ul>
                 </div>
+                <div className="col-2">
+                    <a href={props.pdf} className="material-icons floating-btn boton1">cloud_download</a>
+                </div>
             </div>
-            
+
         </div>
       </div>
       </>
