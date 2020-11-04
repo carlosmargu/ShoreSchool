5f4f864f2955d488868801e1b8f9511a2e706cc4
Carlos Martinez <camgxbox@live.com>
2020-11-03 21:19:56 -0500

    css entregas

--- src/components/Entregas/EntregasComponent.js
+++ src/components/Entregas/EntregasComponent.js
@@ -1,6 +1,8 @@
 import React, {useState,  useEffect}from 'react';
 import firebase from "../../instances/firebase";
 import { v4 as uuid } from 'uuid';
+import { Jumbotron } from 'reactstrap';
+import './EntregasComponent.css';
 
 export default function Entrega() {
     const [pdfUrl, setPdfUrl] = useState([]);
@@ -44,19 +46,36 @@ export default function Entrega() {
 
     return (
         <div>
-            <h1> Subir archivo</h1>
-            <input type="file" onChange={readFile}/>
-            {pdfUrl
-                ? pdfUrl.map(({ id, url }) => {
-                    return (
-                    <div key={id}>
-                         <embed src={url} width="500" height="375" 
-                         type="application/pdf"></embed>
-                        <button onClick={() => deletePdf(id)}>Delete</button>
+            <Jumbotron>
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-8">
+                            <h1 className="tituloentre"> Sección de Entregas</h1>
+                            <p>Sube el documento con la tarea correspondiente a la semana actual</p>
+                            <input type="file" id="BSbtninfo" onChange={readFile}/>
+                             {pdfUrl
+                                ? pdfUrl.map(({ id, url }) => {
+                                    return (
+                                    <div key={id}>
+                                        <embed src={url} width="600" height="600" 
+                                        type="application/pdf"></embed>
+                                        <div className="espaciobtnentre">
+                                        <button className="btnentregas" onClick={() => deletePdf(id)}>Borrar Archivo</button>
+                                        </div>   
+                                    </div>
+                                    );
+                                })
+                                : ''}
+                        </div>
+                        <div class="col-sm">
+                        <img src="https://i.ibb.co/bv9kjbz/Imagen1.png" alt="Imagen1" border="0"/>
+                        </div>
                     </div>
-                    );
-                })
-                : ''}
+                </div>
+            
+            </Jumbotron>
+            
+            
         </div>
     );
 }
